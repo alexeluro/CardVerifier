01c0467b7d166c6cb4865d17b744b9b2f4f67cda
Eluro Alex <alexeluro@gmail.com>
2021-02-22 12:14:21 +0100

    ui implementation

--- app/src/main/java/com/inspiredcoda/cardverifier/ui/MainActivity.kt
+++ app/src/main/java/com/inspiredcoda/cardverifier/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import com.inspiredcoda.cardverifier.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -14,5 +15,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
+
+
+    private fun observer(){
+
+    }
+
 }
